363556c786306f35e6adf50fbeead68b4f390840
Robert Kesterson <rkesters@gmail.com>
2022-03-22 08:51:35 -0400

    initial form

--- src/components/schemaForm.tsx
+++ src/components/schemaForm.tsx
@@ -5,17 +5,21 @@ import { Theme5 as Mui5Theme } from '@rjsf/material-ui';
 
 const Form = withTheme(Mui5Theme);
 import network from '../schema/network.json';
-import {JSONSchema7} from 'json-schema';
+import { JSONSchema7 } from 'json-schema';
+import { useState } from 'react';
 
 const Network: JSONSchema7 = network as JSONSchema7;
-console.dir(Network)
+console.dir(Network);
 
 export function SchemaForm(): JSX.Element {
-	const formData = {};
+	const [formData, setFormData] = useState({});
 	return (
-		<div style={{width: '50%'}}>
-			<Form schema={Network} formData={formData}></Form>
+		<div style={{ width: '50%' }}>
+			<Form
+				schema={Network}
+				formData={formData}
+				onChange={({ formData }) => setFormData(formData)}
+			></Form>
 		</div>
 	);
 }
-
